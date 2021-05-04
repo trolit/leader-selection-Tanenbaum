4e9bb59a6fa4e76134fa45adbedfbb2739b261e1
pawel idzikowski <iddziku@gmail.com>
2021-05-04 13:55:48 +0200

    Create RichTextBoxExtensions.cs

---
+++ lsa-Tanenbaum-app/Extensions/RichTextBoxExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System.Drawing;
+
+namespace System.Windows.Forms
+{
+    // idea from: https://stackoverflow.com/a/1926822
+    public static class RichTextBoxExtensions
+    {
+        public static bool AppendText(this RichTextBox box, DateTime date, string text)
+        {
+            box.SelectionStart = box.TextLength;
+            box.SelectionLength = 0;
+
+            box.SelectionColor = Color.Blue;
+            box.AppendText($"[{GetCurrentTimeStamp(date)}]");
+            box.SelectionColor = box.ForeColor;
+
+            box.SelectionColor = Color.Black;
+            box.AppendText($" {text}\n");
+
+            return true;
+        }
+
+        private static string GetCurrentTimeStamp(DateTime date)
+        {
+            return date.ToString("HH:mm:ss");
+        }
+    }
+}
