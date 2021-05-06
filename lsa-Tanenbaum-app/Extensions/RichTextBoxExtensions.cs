87ac9162214e8253988a6ef35909d69b7f30dc34
pawel idzikowski <iddziku@gmail.com>
2021-05-06 22:00:07 +0200

    Remove date paremeter, calling now, "Now" instead

--- lsa-Tanenbaum-app/Extensions/RichTextBoxExtensions.cs
+++ lsa-Tanenbaum-app/Extensions/RichTextBoxExtensions.cs
@@ -5,13 +5,13 @@ namespace System.Windows.Forms
     // idea from: https://stackoverflow.com/a/1926822
     public static class RichTextBoxExtensions
     {
-        public static bool AppendText(this RichTextBox box, DateTime date, string text)
+        public static bool AppendText(this RichTextBox box, string text)
         {
             box.SelectionStart = box.TextLength;
             box.SelectionLength = 0;
 
             box.SelectionColor = Color.Blue;
-            box.AppendText($"[{GetCurrentTimeStamp(date)}]");
+            box.AppendText($"[{GetCurrentTimeStamp(DateTime.Now)}]");
             box.SelectionColor = box.ForeColor;
 
             box.SelectionColor = Color.Black;
